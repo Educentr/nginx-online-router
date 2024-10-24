557b103a0ffe621ce72bdce7e740edd9e3a17bda
sklukin <sklukin@yandex.ru>
2024-10-24 16:07:01 +0500

    ngx.timer.at -> ngx.timer.every

--- nginx/lua/nginx_onlineconf.lua
+++ nginx/lua/nginx_onlineconf.lua
@@ -1,27 +1,35 @@
-local ngx = require 'ngx'
-
+local ngx = require("ngx")
 
 local nginx_onlineconf = {
-    onlineconf = require("onlineconf"),
-    module = "nginx",
-    timeout = 30
+	onlineconf = require("onlineconf"),
+	module = "nginx",
+	timeout = 30,
 }
 
 local onlineconf = nginx_onlineconf.onlineconf
-if not pcall(function () CONFIG=onlineconf.module( nginx_onlineconf.module ); end) then
-    error("Onlineconf daemon is not running or onlineconf module '" .. nginx_onlineconf.module .. "' does not exist");
+if not pcall(function()
+	CONFIG = onlineconf.module(nginx_onlineconf.module)
+end) then
+	error("Onlineconf daemon is not running or onlineconf module '" .. nginx_onlineconf.module .. "' does not exist")
 end
 
-ngx.log(ngx.INFO, "load config from onlineconf");
+ngx.log(ngx.INFO, "load config from onlineconf")
 
 function nginx_onlineconf.nginx_init_worker()
-    local refresh_timer;
-    refresh_timer = function ()
-        onlineconf.module( nginx_onlineconf.module );
-        ngx.timer.at(nginx_onlineconf.timeout, refresh_timer);
-    end
-    refresh_timer();
+	local function refresh_timer()
+		local ok, err = pcall(function()
+			onlineconf.module(nginx_onlineconf.module)
+		end)
+
+		if not ok then
+			ngx.log(ngx.ERR, "Error in refresh_timer: ", err)
+		end
+	end
+
+	local ok_timer, err_timer = ngx.timer.every(nginx_onlineconf.timeout, refresh_timer)
+	if not ok_timer then
+		ngx.log(ngx.ERR, "Failed to set repeating timer: ", err_timer)
+	end
 end
 
 return nginx_onlineconf
-
